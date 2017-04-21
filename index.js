0461cd41d12c73125c3c6586eb77fb6f14028a1d
CJ <cjzhang88@gmail.com>
2017-04-21 18:27:03 +0800

    测试

--- index.js
+++ index.js
@@ -84,6 +84,10 @@ app.use(function( err, req, res, next){
   })
 })
 
-app.listen(config.port, function(){
-  console.log(`${pkg.name} listening on port ${config.port}`)
-})
+if(module.parent){
+  module.exports = app
+} else{
+  app.listen(config.port, function(){
+    console.log(`${pkg.name} listening on port ${config.port}`)
+  })
+}
